6f7052d7f7d82bdd0c17583ff012810eecb12f82
davidmaniibrahim <ibrahimmanidavid@gmail.com>
2026-01-19 11:18:25 +0100

    first commit

---
+++ exercise/bluelock/script.js
@@ -0,0 +1,12 @@
+function submitForm() {
+  alert("Thank you for contacting BlueLock Academy!");
+}
+
+// window.addEventListener("scroll", () => {
+//   document.querySelectorAll(".scroll-animate").forEach(el => {
+//     const position = el.getBoundingClientRect().top;
+//     if (position < window.innerHeight - 100) {
+//       el.classList.add("show");
+//     }
+//   });
+// });
